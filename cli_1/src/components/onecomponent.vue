cd7cd206e86c2bee02858ec9b3865638da9a0f02
lirunkai <15582889147@163.com>
2016-12-31 01:53:46 +0800

    失败品 请勿模仿

---
+++ cli_1/src/components/onecomponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="firstcomponent">
+    <h1>the first component</h1>
+    <a href="#">write by {{author}}</a>
+  </div>
+
+</template>
+
+
+<script type="text/javascript">
+  export default {
+    data () {
+      return {
+        author: 'lirunkai'
+      }
+    }
+  }
+</script>
+
+<style media="screen">
+  
+</style>
