69087262fb4e908d45bb120b59f1ae8fca223463
lirunkai <15582889147@163.com>
2017-01-08 10:37:17 +0800

    cli-start

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 #vue-demo
 
-#learn the vue 
+#learn the vue
+
+#安装好vue-cli webpack
+
+#vue init wepack 项目名字 创建项目
+
+#npm run dev  运行
--- myapp/src/store/mutation-types.js
+++
@@ -1,4 +0,0 @@
-export const DEC_TOTAL_TIME = 'DEC_TOTAL_TIME'
-export const ADD_TOTAL_TIME = 'ADD_TOTAL_TIME'
-export const DELETE_PLAN = 'DELETE_PLAN'
-export const SAVE_PLAN = 'SAVE_PLAN'
